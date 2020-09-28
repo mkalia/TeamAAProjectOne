c851ed6783066d9eb0f40f9efdd9352b017ee7cf
MohnishKalia <mohnish.kalia@gmail.com>
2020-09-27 21:07:52 -0500

    Changed test cases and added Common folder

---
+++ src/Common/README.md
@@ -0,0 +1,1 @@
+Add `HashTableMap.java` and `MapADT.java` to this `Common` folder, do not change this readme
--- src/Test2/TestVirtualBookTracker.java
+++ src/Test2/TestVirtualBookTracker.java
@@ -1,32 +1,37 @@
 package Test2;
 
+import Backend2.Book;
+import Common.HashTableMap;
+import Common.MapADT;
+
 /**
- * Key Tests:
-Successful import of txt data into app (printed list matches expected data)
-Add lots of books to check performance issues
-Add/remove books and check if the printed list matches
-Change details of books and check if the printed list matches those changes
-*/
+ * Key Tests: Successful import of txt data into app (printed list matches
+ * expected data) Add lots of books to check performance issues Add/remove books
+ * and check if the printed list matches Change details of books and check if
+ * the printed list matches those changes
+ */
 public class TestVirtualBookTracker {
     // Add book with incorrect data (to trigger the front end error messages)
     public static boolean addBookWithBadISBN() {
-        HashTableMap<Long,Book> map = new HashTableMap<>();
-        boolean hasPut = map.put((long) 123, new Book("Apple Johnson", "Farmerville", 2.3));
+        MapADT<Long,Book> map = new HashTableMap<>();
+        long isbn = 123L;
+        boolean hasPut = map.put(isbn, new Book(isbn, "Apple Johnson", "Farmerville", 2));
         return !hasPut;
     }
 
     // Add book with incorrect data (to trigger the front end error messages)
     public static boolean addBookWithBadRating() {
-        HashTableMap<Long,Book> map = new HashTableMap<>();
-        boolean hasPut = map.put(978_3_16_148410_0L, new Book("Paul John", "Thanks and Planes", -1));
+        MapADT<Long,Book> map = new HashTableMap<>();
+        long isbn = 978_3_16_148410_0L;
+        boolean hasPut = map.put(isbn, new Book(isbn, "Paul John", "Thanks and Planes", -1));
         return !hasPut;
     }
 
     // Remove book with empty hash table
     public static boolean removeFromEmptyMap() {
-        HashTableMap<Long,Book> map = new HashTableMap<>();
+        MapADT<Long,Book> map = new HashTableMap<>();
         // String prev = getAllBooks(map);
-        Book nothing = map.remove((long) 12345);
+        Book nothing = map.remove(12345L);
         // String post = getAllBooks(map);
         return nothing == null;// && prev.equals(post);
     }
