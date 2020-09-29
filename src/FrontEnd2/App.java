07dd82f1aeb41400772c72146f0fafaba0a44c20
AlexanderUlate <55561498+AlexanderUlate@users.noreply.github.com>
2020-09-28 21:22:26 -0500

    Add files via upload

--- src/FrontEnd2/App.java
+++ src/FrontEnd2/App.java
@@ -1,4 +1,3 @@
-package FrontEnd2;
 // --== CS400 File Header Information ==--
 // Name: Alexander Ulate
 // Email: ulate@wisc.edu
@@ -6,7 +5,7 @@ package FrontEnd2;
 // TA: Sophie Stephenson
 // Lecturer: Florian Heimerl
 // Notes to Grader:
-// import Common.HashTableMap; // add the HashTableMap
+//import Common.HashTableMap; // add the HashTableMap
 // import Common.MapADT; // Add the Map abstract data type
 import java.util.Scanner; // Get the scanner for the interface
 
@@ -54,21 +53,43 @@ public class App {
   /**
    * This method prints out an error message for an invalid input
    * 
-   * @param error - The number that the error corresponds to
+   * @param error - The error message that needs to be printed
    */
-  private static void error(int error) {
+  private static void error(String error) {
     switch (error) {
-      case 1: // Invalid ISBN number
+      case "ISBN": // Invalid ISBN number
         System.out
-            .println("Invalid Input. Please insert valid ISBN number of 13 decimal numbers: ");
+            .println("INVALID INPUT. Please insert valid ISBN number of 13 decimal numbers");
         break;
-      case 2: // Invalid character
+      case "menu": // Invalid menu select
         System.out
-          .println("Invalid Input. Please insert valid menu select character ('m' to see menu again): ");
+          .println("INVALID INPUT. Please insert valid menu select \ncharacter ('m' to see menu again)");
         break;
       default:
         System.out.println("UNKNOWN INPUT ERROR");
     }
+  }
+  
+  /**
+   * This method checks that the input is valid. The check it does depends on
+   * the check passes into it as a character.
+   *    Title   :   t
+   *    ISBN    :   i
+   *    Rating  :   r
+   *    Author  :   a
+   *    
+   * @param input - The input from the user
+   * @param check - The type of input you are checking
+   * @return Weather it is valid or not
+   */
+  private static boolean checkInput(String input, char check) {
+    switch (check) {
+      case 't':
+        break;
+      default:
+        break;
+    }
+    return false; // Not valid
   }
 
   /**
@@ -84,9 +105,9 @@ public class App {
   private static void printMenu() {
     empty();
     printLine('-', 22);
-    System.out.println("*        MENU        *");
+    System.out.println("|        MENU        |");
     printLine('-', 22);
-    System.out.println("* Type letter to     *\n* select function    *");
+    System.out.println("| Type letter to     |\n| select function    |");
     printLine('-', 22);
     System.out.println("* Add book: a        *");
     System.out.println("* Find book: f       *");
@@ -95,8 +116,27 @@ public class App {
     System.out.println("* Save Tracker: s    *");
     System.out.println("* Quit w/o save: q   *");
     System.out.println("* Print Menu: m      *");
-    printLine('-', 22);
-    System.out.println("Enter function: ");
+  }
+  
+  /**
+   * This method runs the Add book functionality of the app.
+   * 
+   * @param scnr - the scanner
+   */
+  private static void addBook(Scanner scnr) {
+    String title; // The title
+    String isbn; // the ISBN number
+    int rating; // The book rating
+    
+    empty();
+    printLine('*',22);
+    System.out.println("*     ADD A BOOK     *");
+    printLine('*',22);
+    empty();
+    
+    System.out.print("Title of book: ");
+    
+    
   }
 
   /**
@@ -110,6 +150,10 @@ public class App {
     
     Scanner scnr = new Scanner(System.in); // create the scanner
     String input = null; // Create the input
+    boolean end = false; // Helps keep text printing correctly
+    
+    printLine('-', 22);
+    System.out.print("Enter function: ");
     
     // begin the app loop, which also contains the main menu loop
     do {
@@ -117,11 +161,28 @@ public class App {
       
       switch (input) {
         case "a":
+          addBook(scnr);
+          break;
+        case "q":
+          end = true;
+          break;
+        case "m":
+          printMenu();
+          break;
+        default:
+          error("menu");
           break;
       }
-      
-    } while (!scnr.equals("q")); // When q, quit the program
+      if (!end)
+        printLine('-', 22);
+        System.out.print("Enter function: ");
+    } while (!input.equals("q")); // When q, quit the program
     
+    // End the program
+    empty();
+    printLine('-', 61);
+    System.out.println("| CLOSING PROGRAM. Thank you for using the Book Tracker App |");
+    printLine('-', 61);
     scnr.close(); // Close the scanner
 
   }
